8a4748123e173dfbca36f6c57cae2dbb02765b1d
Anh Tuan Nguyen <anhtuank7c@hotmail.com>
2016-12-25 15:57:47 +0700

    add user fields

--- src/services/user/user-model.js
+++ src/services/user/user-model.js
@@ -9,9 +9,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: {type: String, required: true, unique: true},
+  fullName: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  
+
   createdAt: { type: Date, 'default': Date.now },
   updatedAt: { type: Date, 'default': Date.now }
 });
